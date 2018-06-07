525331d7203d0d4e3c1b0d0393739c0527da5aa2
Mohammed <mido5219@gmail.com>
2018-06-07 08:42:04 +0200

    adding get todos route

--- server/server.js
+++ server/server.js
@@ -14,12 +14,20 @@ app.post('/todos', (req, res) => {
     })
 
     todo.save().then((doc, e) => {
-            res.send(doc)
+            res.status(200).send(doc)
         },
         (e) => {
             res.status(200).send(e)
         }
     )
+})
+
+app.get('/todos' , (req,res) =>{
+    Todo.find().then((todos)=>{
+        res.status(200).send({todos})
+    },(e)=>{
+        res.status(400).send(e)
+    })
 })
 
 app.listen(3000, () => {
