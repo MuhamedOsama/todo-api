2af5308cdd040c0890605ee7a2e7f8d9f5678e0c
Mohammed <mido5219@gmail.com>
2018-06-10 05:50:16 +0200

    add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/todos/:id',(req,res) => {
             res.status(404).send()
         }
         if(!todo) {
-            res.status(400).send()
+            res.status(404).send()
         }
         res.send({todo})
     }).catch((e)=>{
