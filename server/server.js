dbc66b66c4364b230c1a6e5331be2adc046a4ed8
Mohammed <mido5219@gmail.com>
2018-06-12 09:06:34 +0200

    add test cases for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -15,34 +15,49 @@ app.post('/todos', (req, res) => {
     })
 
     todo.save().then((doc, e) => {
-            res.send(doc)
+            return res.send(doc)
         },
         (e) => {
-            res.status(400).send(e)
+            return res.status(400).send(e)
         }
     )
 })
 
 app.get('/todos' , (req,res) =>{
     Todo.find().then((todos)=>{
-        res.send({todos})
+        return res.send({todos})
     },(e)=>{
-        res.status(400).send(e)
+        return res.status(400).send(e)
     })
 })
 
 app.get('/todos/:id',(req,res) => {
-    Todo.findById(req.params.id).then((todo)=>{
-        const id = req.params.id
+    const id = req.params.id
+    Todo.findById(id).then((todo)=>{
         if(!ObjectID.isValid(id)){
-            res.status(404).send()
+            return res.status(404).send()
         }
         if(!todo) {
-            res.status(404).send()
+            return res.status(404).send()
         }
         res.send({todo})
     }).catch((e)=>{
-        res.status(400).send()
+        return res.status(400).send()
+    })
+})
+
+app.delete('/todos/:id',(req,res)=>{
+    const id = req.params.id
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!ObjectID.isValid(id)){
+            return res.status(404).send()
+        }
+        if(!todo){
+            return res.status(404).send()
+        }
+        return res.send({todo})
+    }).catch((e)=>{
+        return res.status(404).send()
     })
 })
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -90,3 +90,38 @@ describe('GET /todos/:id',()=>{
         .end(done)
     })
 })
+
+describe('DELETE /todos/:id', ()=>{
+    it('should delete a todo and return it',(done)=>{
+        const id = todos[1]._id.toHexString()
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo._id).toBe(id)
+        })
+        .end((err,res)=>{
+            if(err){
+                 return done(err)
+            }
+            Todo.findById(id).then((todo)=>{
+                expect(todo).toNotExist()
+                done()
+        }).catch((e)=>done(e))
+    })
+})
+    it('should return 404 if todo not found',(done)=>{
+        const id = new ObjectID().toHexString()
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done)
+     })
+    it('should return 404 if object id not valid', (done)=>{
+        const id = 123
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done)
+    })
+})
